5c455d0ef98501f0712ed80cad0cce41b82ef686
iamnotw <32496170+iamnotw@users.noreply.github.com>
2022-11-15 00:14:34 -0600

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -19,7 +19,7 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 
 // To omit an optional field, set it's value to null
 
-// export const managers = [
+ export const managers = [
     // {
     //   "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
     //   "name": "Donald D",
